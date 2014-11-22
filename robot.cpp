37698223acbbfb4295d96400653b03de2391f0cf
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2014-11-22 22:19:27 +0000

    explicacion de como realizar el algoritmo

--- robot.cpp
+++ robot.cpp
@@ -22,5 +22,15 @@ int robot_t::get_Y(){
 }
 
 void robot_t::RecEuristico(cuadrado_t** ){
-  
+ /*
+  En principio cualquier algoritmos que cojamos su solucion
+  sera de tipo recursica, es decir que para saber que camino tenemos
+  que cojer iremos llamando a la funcion segun en numero de posibles
+  caminos que haya(hasta un maximo de 4 caminos(norte,sur, este, oeste)
+  si existiera el caso) para maximizar el rendimiento usaremos una segunda( aun no
+  implementada) que sera para saber que casillas han sido visitadas para no volver a
+  recorrer caminos que ya hemos recorridos. Aun no he pensado como veremos cual
+  es el caminos mas optimo ni como haremos para guardar las posiciones(posiblemente
+  lo haremos con colas (queue) o listas.   
+  */ 
 }
